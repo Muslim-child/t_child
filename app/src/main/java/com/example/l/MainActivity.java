da3e76cdf98b1d60161d5aa3c420d0ba5abc7363
rahma999 <rahmafathy@gmail.com>
2020-03-24 22:06:15 +0200

    tas

---
+++ app/src/main/java/com/example/l/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.l;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img_quran,img_azkar ,img_stories;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img_stories = findViewById(R.id.img_stories);
+    img_stories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast .makeText(MainActivity.this,"ghg",Toast.LENGTH_SHORT).show();
+                Toast .makeText(MainActivity.this,"ghg",Toast.LENGTH_SHORT).show();
+                Toast .makeText(MainActivity.this,"ghg",Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this,Stories.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
